4a52177e70af77184498ef0a4efe1c23ad554d9b
PuidetA <aapuidet@gmail.com>
2024-04-16 19:08:37 +0300

    1. Updated the description with a new one that is more simple but expandable.
    2. Added placeholder functions/methods with descriptions of what each does, what parameters it needs, and what it returns.
    
    3. Added main at the bottom.

--- client.py
+++ client.py
@@ -1,4 +1,4 @@
-"""
+"""OLD: Client-Side Functionality
 1. File Handling
 
 calculateFileHash(filePath): Generates a unique hash (like SHA-1) to identify the file on the network.
@@ -52,3 +52,197 @@ Interaction Example: Downloading a File
 8. Reassembly: reassembleFile() combines chunks into the complete file.
 
 """
+
+
+"""NEW Client-Side Functionality:
+
+Reason for simplified design: The initial design was too complex for the given amount of time. The new design focuses on the core functionalities needed for a basic file-sharing system.
+The focus will be more on P2P sharing between only 2 systems. The client will handle file management, peer management, and file exchange.
+
+1. File Handling
+
+calculateFileHash(filePath): Generates a unique hash (like SHA-1) to identify the file on the network.
+divideFileIntoChunks(filePath, chunkSize): Splits the file into smaller chunks for efficient transfer and resuming downloads.
+reassembleFile(fileHash): Combines chunks into the complete file after downloading.
+
+2. User Interface (UI):
+
+displayFileList(): Updates the UI with the list of files available for download from other peer.
+updateFileList(): Updates the file list to reflect new files that are added to the folder.
+shareFileList(fileList): Triggered when displayFileList() method from another peer requests a list of files.
+updateDownloadProgress(fileHash, progress): Visually displays download progress of a file (or how many chunks are left to be downloaded).
+
+
+3. Peer Management
+
+registerPeer(): Register a peer with given IP address and port number. Both sides have to do it (or only one depending on implementation. Either works.) You can only register one peer right now. (we can add more later on like folder specific peer)
+unregisterPeer(): Unregister the current peer.
+
+
+4. File Exchange
+
+requestChunk(peerID, fileHash, chunkIndex): Send a request to a peer.
+sendChunk(peerID, fileHash, chunkIndex, chunkData): Send a chunk in response.
+downloadFile(fileHash): Get peer list, coordinate requests, and reassemble chunks."""
+
+
+import hashlib # Can be used for generating hash values.
+import os # Can be used for file operations.
+
+
+#TODO: Implement the functions below.
+
+
+
+#TODO: Add a list to store all the available files for sharing. i.e. fileList = []
+
+fileList = []       # List of files available for sharing.
+connectedPeer = ""  # The peer to which the client is connected.
+
+
+#   1. TODO: File Handling
+def calculateFileHash(filePath):
+    """
+    Generates a unique hash (like SHA-1) to identify the file on the network.
+
+    Parameters:
+    - filePath: The path to the file for which the hash is to be calculated. E.g. "C:/Users/User/Documents/file.txt".
+
+    Returns:
+    - hashValue: The calculated hash value for the file. E.g. "a1b2c3d4".
+    """
+
+
+def divideFileIntoChunks(filePath, chunkSize):
+    """
+    Splits the file into smaller chunks for efficient transfer and resuming downloads.
+
+    Parameters:
+    - filePath: The path to the file to be divided into chunks. E.g. "C:/Users/User/Documents/file.txt".
+    - chunkSize: The size (in bytes) of each chunk. E.g. 1024 bytes.
+
+    Returns:
+    - chunkPaths: A list containing the paths to the generated chunks. E.g. ["C:/Users/User/Documents/file_chunk1.txt", "C:/Users/User/Documents/file_chunk2.txt"].
+    """
+
+
+def reassembleFile(fileHash):
+    """
+    Combines chunks into the complete file after downloading.
+
+    Parameters:
+    - fileHash: The hash value identifying the file. E.g. "a1b2c3d4".
+
+    Returns:
+    - filePath: The path to the reassembled file. E.g. "C:/Users/User/Documents/file.txt".
+    """
+
+
+
+#   2. TODO: User Interface (UI)
+def displayFileList():
+    """
+    Updates the UI with the list of files available for download from other peers. E.g. displays a list of files in a window.
+
+    Parameters: None
+
+    Returns: None
+    """
+
+def updateFileList():
+    """
+    Updates the file list to reflect new files that are added to the folder. i.e. a button that triggers the update and adds it to fileList = [].
+
+    Parameters: None
+
+    Returns: None
+    """
+
+def shareFileList(fileList):
+    """
+    Triggered when displayFileList() method from another peer requests a list of files.
+
+    Parameters:
+    - fileList: The list of files available for sharing. E.g. ["file1.txt", "file2.txt"].
+
+    Returns: None
+    """
+
+def updateDownloadProgress(fileHash, progress):
+    """
+    Visually displays download progress of a file (or how many chunks are left to be downloaded).
+
+    Parameters:
+    - fileHash: The hash value identifying the file being downloaded. E.g. "a1b2c3d4".
+    - progress: The download progress, indicating how many chunks are left to be downloaded. E.g. 5/64 chunks downloaded. In the terminal, it can be displayed as "Downloading file: 5/64".
+
+    Returns: None
+    """
+
+
+
+
+#   3. TODO: Peer Management
+def registerPeer():
+    """
+    Register a peer with given IP address and port number.
+    Both sides have to do it (or only one depending on implementation. Either works.).
+    You can only register one peer right now. (we can add more later on like folder specific peer)
+
+    Parameters: None
+
+    Returns: None
+    """
+
+def unregisterPeer():
+    """
+    Unregister the current peer. i.e. make the connectedPeer = "".
+
+    Parameters: None
+
+    Returns: None
+    """
+
+
+
+
+#   4. TODO: File Exchange
+def requestChunk(fileHash, chunkIndex):
+    """
+    Send a request to a peer.
+
+    Parameters:
+    - fileHash: The hash value identifying the file for which the chunk is requested. E.g. "a1b2c3d4".
+    - chunkIndex: The index of the chunk requested. E.g. 0.
+
+    Returns: None
+    """
+
+def sendChunk(fileHash, chunkIndex, chunkData):
+    """
+    Send a chunk in response.
+
+    Parameters:
+    - fileHash: The hash value identifying the file for which the chunk is sent. E.g. "a1b2c3d4".
+    - chunkIndex: The index of the chunk being sent. E.g. 0.
+    - chunkData: The data of the chunk being sent.
+
+    Returns: None
+    """
+
+def downloadFile(fileHash):
+    """
+    Get peer list, coordinate requests, and reassemble chunks.
+
+    Parameters:
+    - fileHash: The hash value identifying the file to be downloaded. E.g. "a1b2c3d4".
+
+    Returns: None
+    """
+
+
+
+#TODO: Add in the functions from above to the main below to make them work.
+# Main
+if __name__ == "__main__":
+    pass
