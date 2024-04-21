6f73e8a81176d705dfcd5b0f41ba940a104a5722
Aino <103110604+aRakkolainen@users.noreply.github.com>
2024-04-21 15:34:56 +0300

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import socket
-import threading
-
-#HOST = "86.50.47.211"
-#socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#socket.create_server(HOST)
-socket = socket.create_connection(("86.50.40.18", 12345))
-socket.send(b"Hello!")
