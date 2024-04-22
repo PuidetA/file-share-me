6665fea497ac761a0271600ab73220da8a96076b
Emma <emmaniemenmaa@gmail.com>
2024-04-22 17:21:57 +0300

    tracker server taas

--- tracker_server.py
+++ tracker_server.py
@@ -13,25 +13,28 @@ from pysondb import db
 connections = {}
 fileDB = db.getDb("fileDB.json")
 
-# Define the server
-HOST = socket.gethostbyname(socket.gethostname()) # This returns the private ip address of the system it is run on.
+# Define and start the server
+HOST = socket.gethostbyname(socket.gethostname())
 PORT = 12345
 ALIVE = False
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST,PORT))
-server.listen() # Number inside specifies how many unexpected connections are allowed before they start getting rejected (Example (5) -> if more than 5 connections are waiting, new requests are denied)
+server.listen(10) # Number inside specifies how many unexpected connections are allowed before they start getting rejected (Example (5) -> if more than 5 connections are waiting, new requests are denied)
 print("Server is listening...")
 print(f"Port: {PORT}")
 print(f"IP: {HOST}")
 
-def clientDisconnect(owner):
-    fileList = fileDB.getByQuery({"owner":owner})
+# Removes disconnected peer's files from the database
+def clientDisconnect(nickname):
+    del connections[nickname]
+    fileList = fileDB.getByQuery({"owner":nickname})
     for file in fileList:
         id = file["id"]
         fileDB.deleteById(id)
     return None
 
+# 
 def sendFileList(client):
     fileList = fileDB.getAll()
     for file in fileList:
@@ -41,21 +44,25 @@ def sendFileList(client):
     return None
 
 
-def handle(client, nickname): # handles the connection
+def handle(client, nickname): # handles the clients and connections
     while True:
         try:
-            message = client.recv(1024).decode("utf-8").split(":") # message in the form: ["request", filehash]
-            if message[0] == "DOWNLOADREQUEST":
+            message = client.recv(1024).decode("utf-8").split(":") # message in the form: "REQUEST:FileHash:FileName" for example
+            
+            if message[0] == "DOWNLOADREQUEST": # Inform uploader for upcoming download request
                 fileHash = message[2]
                 fileDownloader = message[1]
-                fileDownloaderIP, fileDownloaderPort = connections[fileDownloader][1]
-
+                # fileDownloaderIP, Port = connections[fileDownloader][1]
+                fileDownloaderPort = message[3]
+                fileDownloaderIP = message[4]
+                
+                # Getting the uploader information from the database and the connections list:
                 fileDict = fileDB.getByQuery({"hash": fileHash})
                 fileUploader = fileDict[0]["owner"]
 
                 fileRequestMessage = "FILESENDREQUEST:" + fileHash + ":" + fileDownloaderIP + ":" + str(fileDownloaderPort)
                 connections[fileUploader][0].send(fileRequestMessage.encode("utf-8"))
-
+                
 
             elif message[0] == "UPLOADREQUEST":
                 hashList = fileDB.getByQuery({"hash":message[3]})
@@ -80,14 +87,13 @@ def handle(client, nickname): # handles the connection
         except Exception as e:
             print("(handle) Exception occurred:", e)
             clientDisconnect(nickname)
-            del connections[nickname]
             print("3:", connections)
             break
 
-def server_main(connections):
+def server_main():
     while True:
         try:
-            client, address = server.accept() # Communication socket, lets us communicate with the client
+            client, address = server.accept()
             print(f"Connected with {str(address)}")
 
             nickname = client.recv(1024).decode("utf-8")
@@ -96,7 +102,6 @@ def server_main(connections):
             connections[nickname] = [client, address]
 
             print("Nickname of the client is", nickname + "!")
-            # Joskus ku muistetaa ni tehhää databaseluku tai jtn
 
             thread = threading.Thread(target=handle, args=(client, nickname,))
             thread.start() # This starts the worker (handle) function in a separate thread.
@@ -114,6 +119,6 @@ def server_main(connections):
             print("2:", connections)
             continue
 
-server_main(connections)
+server_main()
 
 print("Server is going offline.")
